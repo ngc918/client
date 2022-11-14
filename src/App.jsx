2585f520f08d4bfa34e683917fdda797e007fde2
ngc.918 <nicolas.g9182@gmail.com>
2022-11-13 23:56:13 -0500

    day 1

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+
+import { ChannelListContainer, ChannelContainer } from "./components";
+
+const apiKey = "8xseed2nyuhp";
+
+const client = StreamChat.getInstance(apiKey);
+
+const App = () => {
+	return (
+		<div className="app_wrapper">
+			<Chat client={client} theme="team light">
+				<ChannelListContainer />
+				<ChannelContainer />
+			</Chat>
+		</div>
+	);
+};
+
+export default App;
---
+++ src/components/ChannelListContainer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChannelListContainer = () => {
+	return <div>ChannelListConatiner</div>;
+};
+
+export default ChannelListContainer;
